388f604c09d5e8d637d777e80882eec3fb935784
Sieva-cmd <musyokasieva@gmail.com>
2022-06-15 16:21:46 +0300

    my fisrt react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react';
+import './App.css'
+import React, {useEffect} from 'react';
+import SearchIcon from './Search.svg'
+import MovieCard from './MovieCard';
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=eebe598a'
+ const movie1 ={
+  Poster: "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
+  Title: "Italian Spiderman",
+  Type: "movie",
+  Year: "2007",
+  imdbID: "tt2705436"
+ }
+const  App = () => {
+  const [movies, setMovies] =useState([]);
+  const[searchTerm, setSearchTerm] =useState('')
+
+  const searchMovies = async (title) =>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search)
+
+  }
+  useEffect(() =>{
+    searchMovies('Spiderman');
+
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> MovieLand</h1>
+      <div className='search'>
+        <input  
+        placeholder='Search for movies' 
+        value={searchTerm}
+         onChange={(e) =>setSearchTerm(e.target.value)}/>
+
+         <img src= {SearchIcon } 
+         alt ='search'
+         onClick={ () => searchMovies(searchTerm)}
+         />
+      </div>
+
+      {
+        movies?.length > 0 
+        ? (
+          <div  className='container'>
+              {movies.map((movie) =>(
+                <MovieCard movie ={movie} />
+
+              )
+                  
+
+              )}
+        
+   
+          </div>
+         )
+        :
+        (
+          <div className='empty'>
+            <h2> No movies Found</h2>
+          </div>
+        )
+      }
+    
     </div>
   );
-}
+} 
 
 export default App;
